7ea5df830761dc1fab98a4f4777ae3c4494f6b5b
Ettikus <141125542+Ettikus@users.noreply.github.com>
2024-06-24 10:43:53 +0200

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,14 @@
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register";  // Importing your Register component
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/register" element={<Register />} />  {/* New route for Register component */}
+      <Route path="/register" element={<Register />} /> 
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
