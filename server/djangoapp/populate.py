94b97b1ade9192936ec8b03e6120b8c80cf01210
Ettikus <141125542+Ettikus@users.noreply.github.com>
2024-07-03 10:18:08 +0200

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -53,3 +53,4 @@ def initiate():
         CarModel.objects.create(name=data['name'], car_make=data['car_make'],
                                 type=data['type'], year=data['year'])
     pass
+    
