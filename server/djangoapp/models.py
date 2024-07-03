3f3729d571baf3abcdaa1c3518889b0c633f8bc5
Ettikus <141125542+Ettikus@users.noreply.github.com>
2024-07-03 10:16:21 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -5,7 +5,9 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    
     # Additional fields if needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -14,18 +16,25 @@ class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     dealer_id = models.IntegerField()  # Refers to a dealer created in Cloudant database
     name = models.CharField(max_length=100)
+    
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
+    
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
+
     # Additional fields if needed
+
     def __str__(self):
         return f"{self.car_make.name} {self.name}"  # Return car make and model name as the string representation
+
