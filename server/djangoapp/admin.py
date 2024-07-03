1c41c0ce88449d54ecfc4523931455ddbacd0a01
Ettikus <141125542+Ettikus@users.noreply.github.com>
2024-07-03 11:20:45 +0200

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -6,17 +6,20 @@ class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 1
 
+
 # CarModelAdmin class to customize the admin interface for CarModel
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ['name', 'type', 'year', 'car_make']
     list_filter = ['type', 'year', 'car_make']
     search_fields = ['name', 'car_make__name']
 
+
 # CarMakeAdmin class with CarModelInline to display related CarModels within CarMake admin
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
     list_display = ['name', 'description']
     search_fields = ['name']
+
 
 # Registering models with their respective admins
 admin.site.register(CarMake, CarMakeAdmin)
